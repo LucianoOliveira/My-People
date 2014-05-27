c1820abcb1a0b35621144a51e185b1e54b6a5c32
Luciano Oliveira <luciano8.oliveira@gmail.com>
2014-05-27 19:19:39 +0100

    New Files included
    Previous version had only references to these files, now I have
    included the files itself on the bundle

---
+++ Supporting Files/NSString+converterCSV.h
@@ -0,0 +1,16 @@
+//
+//  NSString+converterCSV.h
+//  My People
+//
+//  Created by Luciano Oliveira on 21/05/14.
+//  Copyright (c) 2014 B-ON Engineering. All rights reserved.
+//
+
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (converterCSV)
+
+-(NSArray *)csvRows;
+
+@end
